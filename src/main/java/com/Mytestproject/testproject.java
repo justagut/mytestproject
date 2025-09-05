3edb65b71e4d201cd7f2ee0b0f09c02aa0aa9fe7
justagut <jortsanderman@gmail.com>
2025-09-05 17:03:12 +0200

    adding items

--- src/main/java/com/Mytestproject/testproject.java
+++ src/main/java/com/Mytestproject/testproject.java
@@ -30,7 +30,7 @@ public class testproject {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in.
+        // Register ourselves for server and other game events we are interested in. hihi
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
